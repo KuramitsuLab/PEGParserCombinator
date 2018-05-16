3f430718e5d573803e1e8a7930a6ba14e47d78b3
tadataku-lab <tadataku.lab@gmail.com>
2018-05-16 14:50:21 +0900

    fet: define parse function and ParserContext

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+#[derive(Debug)]
 enum Exp{
     Empty,
     Char{c:char},
     AnyChar,
-    Symbol{sym:&`static str},
+    Symbol{sym:&'static str},
     Seq{e1:Box<Exp>, e2:Box<Exp>},
     Choice{e1:Box<Exp>, e2:Box<Exp>},
     Rep{e:Box<Exp>},
@@ -10,8 +11,19 @@ enum Exp{
     Not{e:Box<Exp>},
 }
 
+impl Exp{
+    fn parse(&self, p: &mut ParserContext) -> bool{
+        match self {
+            &Exp::Empty => true,
+            _ => panic!("{:?}: This expression is undefined!", self)
+        }
+    }
+}
 
+struct ParserContext{
+    input: Vec<u8>,
+}
 
 fn main() {
-    println!("Hello, world!!!!");
+    println!("{}", Exp::Empty.parse(&mut ParserContext{input: "hello".to_string().into_bytes()}));
 }
---
+++ target/debug/deps/test1-1326d406d02813ad.d
@@ -0,0 +1,5 @@
+/Users/macmini-a/Lab/project/PEGParserCombinator/target/debug/deps/test1-1326d406d02813ad: src/main.rs
+
+/Users/macmini-a/Lab/project/PEGParserCombinator/target/debug/deps/test1-1326d406d02813ad.d: src/main.rs
+
+src/main.rs:
