3436519d3ec077ef973b3eca924265fa4a8f0017
nakamura tomone <tomone@nakamura-no-MacBook-Air.local>
2018-05-17 16:28:51 +0900

    passed char1 and anychar1 (tentative)

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,51 @@ pub enum Exp{
     Opt{e:Box<Exp>},
     Not{e:Box<Exp>},
 }
-
+/*
+Expの修正とparse関数の追加、ParserContextの追加を行いました。
+このparser関数でマッチされるExpを増やすようにして開発を進めていきましょう。
+とりあえずEmptyだけ書いておきました。(未完成)
+*/
 impl Exp{
     pub fn parse(&self, p: &mut ParserContext) -> bool{
         match self {
             &Exp::Empty => true,
+            &Exp::Char{ref c} => {
+                p.pos += 1;
+                true  //空だとfalse
+            },
+            &Exp::AnyChar => {
+                p.pos += 1;
+                true
+            },  //空だとfalse
+//            &Exp::Symbol{ref sym} =>,
+            &Exp::Seq{ref e1, ref e2} =>{
+                if {
+                    p.pos += 1;
+                    true
+                }else{
+                    false
+                }
+            },
+/*            &Exp::Choice{ref e1, ref e2} =>{
+                if {
+                    true
+                }else if {
+                    true
+                }else{
+                    false
+                }
+            },
+            &Exp::Rep{ref e} =>,
+            &Exp::Opt{ref e} =>{
+                if{
+
+                }else{
+                    true
+                }
+            },
+            &Exp::Not{ref e} =>,
+*/
             _ => panic!("{:?}: This expression is undefined!", self)
         }
     }
