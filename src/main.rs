b78c26a756f1135bad9910e8393b048c5da3e767
nkmrtmn <tomone.nkme24@gmail.com>
2018-06-15 23:12:50 +0900

    impl tree in ParserContext

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,26 @@ use peg_parser_combinator::*;
 fn main() {
 //    println!("{}", Exp::Empty.parse(&mut ParserContext::new("hello".to_string().into_bytes(),HashMap::new())));
 
-    let s: &'static str = "((1))";
+    let s: &'static str = "((((((((((1))))))))))";
     let mut rules = HashMap::new();
-    let mut child = Vec::new();
     rules.insert("P",choice(seq(char('('),seq(sym("A"),char(')'))),char('1')));
     rules.insert("A",choice(seq(sym("P"),seq(char('+'),sym("A"))),choice(seq(sym("P"),seq(char('-'),sym("A"))),sym("P"))));
     let mut p: ParserContext = ParserContext::new(String::from(s).into_bytes(),rules.clone());
-    rules.get(&"A").unwrap().parse(&mut p,&mut child);
+    Exp::Symbol{sym:&"A"}.parse(&mut p);
 
-    println!("{}",child[0].two_string());
- //   println!("memosu{:?}",p.memos);
+
+/*
+    let s: &'static str = "abc";
+    let mut rules = HashMap::new();
+        rules.insert("S",seq(sym("A"),sym("BC")));
+        rules.insert("A",char('a'));
+        rules.insert("BC",seq(char('b'),char('c')));  
+     let mut p: ParserContext = ParserContext::new(String::from(s).into_bytes(),rules.clone());
+//    Exp::Symbol{sym:&"A"}.parse(&mut p,&mut child);
+    Exp::Symbol{sym:&"S"}.parse(&mut p);
+*/
+    println!("{}",p.tree[0].two_string());
+//    println!("memosu{:?}",p.memos);
 
         
 }
